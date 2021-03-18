2a924d82a73c1018fe35873012cf0f8ca6e20d05
danaremar <danaremar@alum.us.es>
2021-03-18 20:18:10 +0100

    First view of the app
    I created a Menu and prepared install files.

--- angular-installation/install.cmd
+++ angular-installation/install.cmd
@@ -1,1 +1,3 @@
 npm install -g @angular/cli
+npm install bootstrap
+ng add @angular/material
--- yourney-angular-app/src/app/app.module.ts
+++ yourney-angular-app/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
