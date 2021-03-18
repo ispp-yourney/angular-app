49a4a090bd1d9e047af47e951b5d2685247fe931
muzta <javyngs@gmail.com>
2021-03-18 21:37:00 +0100

    Listado Itinerario
    Primera subida del listado de itinerario probando la herramienta

--- angular-installation/install.cmd
+++ angular-installation/install.cmd
@@ -1,3 +1,5 @@
+cd ..
+cd yourney-angular-app
 npm install -g @angular/cli
 npm install bootstrap
 ng add @angular/material
--- yourney-angular-app/src/app/app.module.ts
+++ yourney-angular-app/src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import { ItinerarylistComponent } from './components/itinerarylist/itinerarylist.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ItinerarylistComponent
   ],
   imports: [
     BrowserModule,
+    MatSliderModule,
     AppRoutingModule,
     NoopAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
