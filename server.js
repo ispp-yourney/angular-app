167cf403dc2eeab5a390c9aa468fb3e6e487ab88
edubotdom <55277082+edubotdom@users.noreply.github.com>
2021-03-19 02:12:00 +0100

    Cambiada ruta relativa de despliegue
    Tarea relativa: #2

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + 'yourney-angular-app/dist/yourney-angular-app'));
+app.use(express.static(__dirname + 'dist/yourney-angular-app'));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'yourney-angular-app/dist/yourney-angular-app/index.html'));
+  res.sendFile(path.join(__dirname + 'dist/yourney-angular-app/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
