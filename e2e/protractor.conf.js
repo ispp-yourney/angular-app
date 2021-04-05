ba2890b9eb47cef502a76159d1ac6389b107e308
juanogtir <juanogtir@alum.us.es>
2021-04-05 02:44:35 +0200

    Cambio chromedriver

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -19,7 +19,7 @@ exports.config = {
   directConnect: false,
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://localhost:4200/',
-  chromeDriver:'../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_89.**',
+  chromeDriver:'../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_89.0.4389.23',
   framework: 'jasmine2',
   jasmineNodeOpts: {
     showColors: true,
