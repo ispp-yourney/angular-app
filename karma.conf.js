b6be6f96931532f3e0e28da6e0f0a862e8274600
juanogtir <juanogtir@alum.us.es>
2021-04-14 00:24:32 +0200

    Tests de servicio TokenService
    Se han realizado algunos tests del servicio de token
    
    Tarea relacionada: #71

--- karma.conf.js
+++ karma.conf.js
@@ -1,44 +1,50 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// // Karma configuration file, see link for more information
+// // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+          require('karma-jasmine'),
+          require('karma-chrome-launcher'),
+          require('karma-jasmine-html-reporter'),
+          require('karma-coverage-istanbul-reporter'),
+          require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+          clearContext: false // leave Jasmine Spec Runner output visible in browser
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/yourney-angular-app'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+      coverageIstanbulReporter: {
+          dir: require('path').join(__dirname, './coverage/yourney'),
+          reports: ['html', 'lcovonly', 'text-summary'],
+          fixWebpackSourcePaths: true
+      },
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['ChromeHeadless'],
+      customLaunchers: {
+          MyHeadlessChrome: {
+              base: 'ChromeHeadless',
+              flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223', '--watch=false']
+          },
+          ChromeHeadlessNoSandbox: {
+              base: 'ChromeHeadless',
+              flags: ['--no-sandbox']
+          },
+          ChromeHeadlessCI: {
+              base: 'ChromeHeadless',
+              flags: ['--no-sandbox']
+          }
+      },
+      singleRun: false,
+      restartOnFileChange: true,
+      captureTimeout: 210000,
+      browserDisconnectTolerance: 3,
+      browserDisconnectTimeout : 210000,
+      browserNoActivityTimeout : 210000
   });
 };
