2ffec2d840dfcefd068fb095a4aaaa85c860de68
muzta <javyngs@gmail.com>
2021-04-18 12:22:46 +0200

    Admin puede modificar los landmark
    Un administrador es el unico que puede modificar los landmark
    
    Tarea relacionada: #97

--- src/app/components/landmark/landmark-show/landmark-show.component.ts
+++ src/app/components/landmark/landmark-show/landmark-show.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Landmark } from 'src/app/models/itinerary';
+import { Landmark, LandmarkDto } from 'src/app/models/itinerary';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { LandmarkService } from 'src/app/services/landmark.service';
 import { TokenService } from 'src/app/services/token.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landmark-show',
@@ -11,7 +12,7 @@ import { TokenService } from 'src/app/services/token.service';
 })
 export class LandmarkShowComponent implements OnInit {
 
-  constructor(private landmarkService:LandmarkService,private route: ActivatedRoute, private tokenService: TokenService, private router: Router,) { }
+  constructor(private landmarkService:LandmarkService,private route: ActivatedRoute, private tokenService: TokenService, private router: Router, private formBuilder: FormBuilder) { }
 
   landmark:Landmark;
   isAdmin:boolean;
@@ -20,26 +21,61 @@ export class LandmarkShowComponent implements OnInit {
   loaded: boolean = false;
   currentlySponsored = false;
   endSponsoredDate = null;
+  editForm: FormGroup;
+  tieneActividades: boolean = true;
 
   ngOnInit(): void {
+    this.isAdmin=this.tokenService.getAuthorities()[0]['authority'] == 'ROLE_ADMIN'
+    if(this.isAdmin){
+      this.editForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        description:['', Validators.required],
+        price: ['0', Validators.min(0)],
+        country: [''],
+        category: [''],
+        city: [''],
+        latitude: ['', [Validators.min(-90), Validators.max(90)]],
+        longitude: ['', [Validators.min(-180), Validators.max(180)]],
+        email: ['', [Validators.email,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+        phone: ['', Validators.pattern("^[+]*\\([0-9]{1,4}\\)[-\\s\\./0-9]*$")],
+        website: ['', Validators.pattern("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$")],
+        instagram: ['', Validators.pattern("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$")],
+        twitter: ['', Validators.pattern("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$")]
+      })
+    }
     this.loadLandmark();
   }
 
   loadLandmark(): void {
     this.landmarkService.mostrar(Number(this.route.snapshot.paramMap.get('id'))).subscribe(
       data => {
-     console.log(data)
+        console.log(data)
         this.landmark = data;
-        this.isAdmin=this.tokenService.getAuthorities().includes('admin')
+        this.landmarkService.tieneActividades(this.landmark.id).subscribe(data => this.tieneActividades = data);
         this.containError = false
         this.loaded = true;
-        console.log("Fecha actual " + String(new Date()))
-        console.log("Fecha caducidad " + String(new Date(this.landmark.endPromotionDate)))
-        console.log("Esta sponsorizado? " + (new Date(this.landmark.endPromotionDate) > new Date()))
         if(new Date(this.landmark.endPromotionDate) > new Date()){
           this.currentlySponsored = true;
           this.endSponsoredDate = (new Date(this.landmark.endPromotionDate)).toLocaleString();
         }
+
+        if(this.isAdmin) {
+          console.log("Primer paso")
+          this.editForm.controls['name'].setValue(data.name);
+          this.editForm.controls['description'].setValue(data.description);
+          this.editForm.controls['price'].setValue(data.price);
+          this.editForm.controls['country'].setValue(data.country);
+          this.editForm.controls['city'].setValue(data.city);
+          this.editForm.controls['latitude'].setValue(data.latitude);
+          this.editForm.controls['longitude'].setValue(data.longitude);
+          this.editForm.controls['category'].setValue(data.category);
+          this.editForm.controls['email'].setValue(data.email);
+          this.editForm.controls['phone'].setValue(data.phone);
+          this.editForm.controls['website'].setValue(data.website);
+          this.editForm.controls['instagram'].setValue(data.instagra,);
+          this.editForm.controls['twitter'].setValue(data.twitter);
+        }
+
       },
       err => {
         var returned_error = err.error.text
@@ -62,6 +98,50 @@ export class LandmarkShowComponent implements OnInit {
       }
     )
     
+  }
+
+  onUpdate() {
+    const wait = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout( () => {
+         resolve( this.router.navigate(['/punto_interes/' + this.landmark.id]).then( () => {window.location.reload()} ))
+        }, 500)
+      })
+    };
+
+    //Actualizar landmark
+    var editedLandmark = new LandmarkDto(
+      this.landmark.id,
+      this.editForm.value.name,
+      this.editForm.value.description,
+      this.editForm.value.price,
+      this.landmark.country,
+      this.editForm.value.city,
+      this.editForm.value.latitude,
+      this.editForm.value.longitude,
+      this.landmark.category,
+      this.editForm.value.email,
+      this.editForm.value.phone,
+      this.editForm.value.website,
+      this.editForm.value.instagram,
+      this.editForm.value.twitter,
+      0);
+    this.landmarkService.updateLandmark(editedLandmark).subscribe(
+      data => {
+        wait()
+      }, err => {
+        
+      }
+    )
+  }
+
+  onDelete(){
+    this.landmarkService.deleteLandmark(this.landmark.id).subscribe(data => {
+      this.router.navigateByUrl('/buscadorLandmark')
+    }, err => {
+      // console.log(err)
+    })
+  
   }
 
 }
