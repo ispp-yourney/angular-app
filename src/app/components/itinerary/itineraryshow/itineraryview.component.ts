5225ebc8638324ea3e4f1011a581aa882d6d9c1f
muzta <javyngs@gmail.com>
2021-04-18 12:21:17 +0200

    Admin edita y borra itinerarios
    Un administrador puede modificar cualquier itinerario y borrar aquellos
    que no sean suyos, expecto que esten relacionados con algun itinerario
    
    Tarea relacionada: #97

--- src/app/components/itinerary/itineraryshow/itineraryview.component.ts
+++ src/app/components/itinerary/itineraryshow/itineraryview.component.ts
@@ -20,6 +20,7 @@ export class ItineraryViewComponent implements OnInit  {
   containError: boolean = false;
   messageError: string;
   loadedItineraries:boolean;
+  isAdmin:boolean = false;
 
   ngOnInit(): void {
     this.loadItinerary()
@@ -34,6 +35,7 @@ export class ItineraryViewComponent implements OnInit  {
         this.loadDays()
         this.listNumberDays = Array.from({length: Object.keys(this.days).length}, (_, i) => i + 1)
         this.isMyItinerary = (this.tokenService.getUsername() != null) && (this.tokenService.getUsername().length>0) && (this.tokenService.getUsername() == this.itinerary.username)
+        this.isAdmin = this.tokenService.getAuthorities()[0]['authority'] == 'ROLE_ADMIN';
         this.containError = false
         this.loadedItineraries = true;
       },
