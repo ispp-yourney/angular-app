6f6045c8e55a928dbe772e4e0a121a04d61a8270
danaremar <danaremar@alum.us.es>
2021-04-24 11:42:12 +0200

    Se añade Términos y Condiciones
    Se crea una nueva ventana en la cuál se mostrará toda la información relacionada con "Términos y Condiciones", así como todo lo relacionado con los asuntos/trámites legales, tal y como dicta el reglamento GDPR.
    
    Se añade un enlace desde el menú (Legal) y otro desde la ventana de registro a la aplicación.
    
    Tarea relacionada #126

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import {PermissionsService as guard } from './revisors/permissions.service';
 import { ItineraryupdateComponent } from './components/itinerary/itineraryupdate/itineraryupdate.component';
 import { BuscadorLandmarkComponent } from './components/buscador-landmark/buscador-landmark.component';
 import { LandmarkShowComponent } from './components/landmark/landmark-show/landmark-show.component';
+import { TermComponent } from './components/terms/terms.component';
 
 export const routes: Routes = [
   {path: '' , component: IndexComponent},
@@ -27,6 +28,7 @@ export const routes: Routes = [
   {path: 'error', component: NotFoundComponent},
   {path:'perfil/:username', component: ProfileComponent, pathMatch: 'full'},
   {path:'perfil/:username/itinerarios/:id', component: ItinerarylistComponent,pathMatch: 'full'},
+  {path: 'terminos', component: TermComponent, pathMatch: 'full'},
   {path: '**', component: NotFoundComponent}
 ];
 
